220934c76e11ff12b475b3a6aec1a7a376cb48fa
Joaomarcellodev <joaomarcellobraz@gmail.com>
2025-07-08 13:07:56 -0300

    settings estructure project

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+
+
+export function Logo() {
+    return (
+        <div className={styles.conteiner}>
+            <img src="" alt="" />
+        </div>
+    )
+}
